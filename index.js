3ce84b3c2dd9de790e7ba392913de2f189672fda
Daniel Högel <hoegel.daniel@gmail.com>
2019-08-13 19:40:40 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/**
+ * VARIABLES
+ */
+
+const URL = 'https://zeit.de'
+const INTERVAL = 1000; // in ms (1000 = 1s)
+const FILENAME = 'log.text';
+
+
+/**
+ * IMPORTS
+ */
+
+const axios = require('axios');
+const fs = require('fs');
+
+
+/**
+ * HELPER FUNCTIONS
+ */
+
+function log(...args) {
+    const timestamp = new Date().toISOString()
+    const message = timestamp + '\t' + args.join('\t');
+    stream.write(message + '\n');
+    console.log(message);
+}
+
+function testConnection() {
+    axios.get(URL)
+        .then(response => {
+            if (response.status === 200) {
+                log('ON', response.status, response.statusText);
+            } else {
+                log('OFF', response.status, response.statusText);
+            }
+        })
+        .catch(error => {
+            log('ERROR', error.code);
+        });
+}
+
+
+/**
+ * MAIN
+ */
+
+const stream = fs.createWriteStream(FILENAME, { flags:'a' });
+setInterval(testConnection, INTERVAL);
